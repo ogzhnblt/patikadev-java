baf786594ac8241fa597e01ab89e3f9b05b7fd03
oguzhanabalta <oguzhanbalta99@gmail.com>
2024-03-30 18:00:18 +0300

    nt ortalamasi eklendi

---
+++ src/exmaple1/NotOrtalamasi.java
@@ -0,0 +1,34 @@
+package exmaple1;
+
+import java.util.Scanner;
+
+public class NotOrtalamasi {
+    public static void main(String[] args) {
+        int mat, fizik, kimya, turkce, tarih, muzik;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Matematik notunuz:");
+        mat = input.nextInt();
+
+        System.out.print("Fizik notunuz:");
+        fizik = input.nextInt();
+
+        System.out.print("Kimya notunuz:");
+        kimya = input.nextInt();
+
+        System.out.print("Turkce notunuz:");
+        turkce = input.nextInt();
+
+        System.out.print("Tarih notunuz:");
+        tarih = input.nextInt();
+
+        System.out.print("Muzik notunuz:");
+        muzik = input.nextInt();
+
+        int toplam = mat + fizik + kimya + turkce + tarih + muzik;
+        double sonuc = toplam / 6.0;
+        System.out.println("Ortalamanız : " + sonuc);
+
+    }
+}
